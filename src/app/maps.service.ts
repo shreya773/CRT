dc56d85ff311489fd0215a79e1819f2ab2c48751
shreya773 <64827498+shreya773@users.noreply.github.com>
2020-05-31 21:06:35 +0530

    changes

--- src/app/maps.service.ts
+++ src/app/maps.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
+interface Location{
+  latitude:string;
+  longitude:string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapsService {
 
- 
+  constructor(private http: HttpClient) {}
+
+  getLocation(){
+    return this.http.get<Location>('http://api.ipapi.com/api/check?access_key=5d1c52d2a6c92e4d2a1d1a8bfd8d699f')
+    
+  }
     
 }
 
--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapsService } from '../maps.service';
 
 @Component({
   selector: 'app-maps',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
-  constructor() { }
+  lat:string='';
+  lng:string='';
+
+  location:Object;
+  
+  constructor(private map: MapsService) { }
 
   ngOnInit() {
+    this.map.getLocation().subscribe( data =>{
+      console.log(data);
+      this.lat = data.latitude;
+      this.lng = data.longitude;
+    })
   }
 
 }
